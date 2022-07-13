5542c11efed3bd0271d99977f86a834c5e3ce989
Michal Budkowski <michalbudkowski88@gmail.com>
2022-07-13 19:33:51 +0200

    Add product instantion

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,19 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct', testProduct);
+      const thisApp = this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource
     },
 
     init: function(){
@@ -74,6 +85,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
