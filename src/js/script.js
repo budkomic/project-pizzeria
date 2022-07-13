99510f20d367217438149a1cd681b148f8a10fa1
Michal Budkowski <michalbudkowski88@gmail.com>
2022-07-13 20:41:54 +0200

    Add product render

--- src/js/script.js
+++ src/js/script.js
@@ -53,13 +53,35 @@
   };
 
   class Product{
-    constructor(id, data){
+    constructor(id, data) { 
       const thisProduct = this;
 
       thisProduct.id = id;
       thisProduct.data = data;
 
-      console.log('new Product:', thisProduct);
+      thisProduct.renderInMenu(); 
+      thisProduct.getElements();
+      thisProduct.initAccordion(); 
+      thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
+      thisProduct.processOrder();
+      // console.log('new product', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // console.log('GenarateHTML is:', generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
