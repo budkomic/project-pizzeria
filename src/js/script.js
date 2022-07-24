4233b1bbf2ece7e5ad4a0ce49233be1ce56df3c8
Michal Budkowski <michalbudkowski88@gmail.com>
2022-07-24 20:15:40 +0200

    change accrodion

--- src/js/script.js
+++ src/js/script.js
@@ -131,24 +131,26 @@
 
     }
 
-    initAccordion() {
+    initAccordion(){
       const thisProduct = this;
-
+		
       /* find the clickable trigger (the element that should react to clicking) */
-      // const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+      // const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
-      thisProduct.accordionTrigger.addEventListener('click', function (event) {
-
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
+      
         /* prevent default action for event */
         event.preventDefault();
 
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct !== null) {
+        if(activeProduct != thisProduct.element && activeProduct != null){
           activeProduct.classList.remove('active');
         }
+		
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
       });
